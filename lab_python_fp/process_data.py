30846bc6c6108fbc12a92087cb3afda635197b1f
Никита Мальков <nikitamalkov@MacBook-Pro--ak3la.local>
2025-12-07 14:36:44 +0300

    lab3-4

---
+++ lab_python_fp/process_data.py
@@ -0,0 +1,36 @@
+import json
+import os
+from print_result  import print_result
+from cm_timer import cm_timer_1
+from gen_random import gen_random
+from unique import Unique
+
+path = os.path.join(os.path.dirname(__file__), "data_light.json")
+
+with open(path) as f:
+    data = json.load(f)
+
+@print_result
+def f1(vacancies):
+    jobs = [vacancy["job-name"] for vacancy in vacancies]
+    return sorted(Unique(jobs, case_sensitive=False))
+
+@print_result
+def f2(jobs):
+    return list(filter(lambda job: isinstance(job, str) and job.lower().startswith("программист"), jobs))
+
+@print_result
+def f3(jobs):
+    return list(map(lambda job: job + " с опытом Python", jobs))
+
+@print_result
+def f4(jobs):
+    salaries = gen_random(len(jobs), 100_000, 200_000)
+    result = []
+    for job, salary in zip(jobs, salaries):
+        result.append(f"{job}, зарплата {salary} руб.")
+    return result
+
+if __name__ == '__main__':
+    with cm_timer_1():
+        f4(f3(f2(f1(data))))
---
+++ lab_python_fp/unique.py
@@ -0,0 +1,52 @@
+from typing import Iterable, Any
+from gen_random import gen_random
+
+class Unique(object):
+    def __init__(self, data: Iterable[Any], case_sensitive: bool=True):
+        self.sequence = iter(data)
+        self.used_elements = dict()
+        self.case_sensitive = case_sensitive
+
+    def __next__(self) -> Any:
+        while True:
+            element = next(self.sequence)
+
+            if isinstance(element, str) and not self.case_sensitive:
+                key = element.lower()
+            else:
+                key = element
+
+            if key not in self.used_elements:
+                self.used_elements[key] = True
+                return element
+
+    def __iter__(self):
+        return self
+    
+def main():
+    print('---')
+
+    for item in Unique([1, 1, 1, 1, 1, 2, 2, 2, 2, 2]):
+        print(item)
+
+    print('---')
+
+    for item in Unique(gen_random(10, 1, 3)):
+        print(item)
+
+    data = ['Apple', 'Banana', 'apple', 'Orange', 'banana', 'Grape']
+
+    print('---')
+
+    for item in Unique(data, case_sensitive=False):
+        print(item)
+
+    print('---')
+
+    for item in Unique(data, case_sensitive=True):
+        print(item)
+    
+    return 0
+
+if __name__ == "__main__":
+    main()
